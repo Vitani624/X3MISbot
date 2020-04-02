88913ebf7ddf2b3d77c0008a6fc6ef05c14c7fb9
Vitani <45301629+Vitani624@users.noreply.github.com>
2020-04-02 10:57:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -117,6 +117,17 @@ client.on("message", async message => {
     // And we get the bot to say the thing: 
   message.channel.send(message.author + " *laughs at* " + args[0]);
   }
+  if(command === "announce") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{});
+      if(!message.member.roles.some(r=>["Administrator", "Moderator","Alpha","Elder"].includes(r.name)) )
+      return message.reply("Access denied!");
+    // And we get the bot to say the thing: 
+    message.channel.send("@everyone" + sayMessage);
+  }
   if(command === "kick") {
     // This command must be limited to mods and admins. In this example we just hardcode the role names.
     // Please read on Array.some() to understand this bit: 

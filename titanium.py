9969d5209a033aa1cb0b32a94de28f7c5b7a5079
Vitani <45301629+Vitani624@users.noreply.github.com>
2020-05-29 16:27:17 +0200

    Update titanium.py

--- titanium.py
+++ titanium.py
@@ -21,37 +21,37 @@ async def on_ready():
 async def on_member_join(member):
     ctar = client.get_channel(561044806919520256)
     membercount = len(ctar.guild.members and not member.bot)
-    await ctar.edit(name = f`Members:{membercount}`)
+    await ctar.edit(name = f'Members:{membercount}')
 
 @client.event
 async def on_member_remove(member):
     ctar = client.get_channel(561044806919520256)
     membercount = len(ctar.guild.members and not member.bot)
-    await ctar.edit(name = f`Members:{membercount}`)
+    await ctar.edit(name = f'Members:{membercount}')
 
 @client.event
 async def on_guild_channel_create(channel):
     ctar = client.get_channel(561042915011592222)
     channelcount = len(ctar.guild.channels)
-    await ctar.edit(name = f`Channels:{channelcount}`)
+    await ctar.edit(name = f'Channels:{channelcount}')
 
 @client.event
 async def on_guild_channel_delete(channel):
     ctar = client.get_channel(561042915011592222)
     channelcount = len(ctar.guild.channels)
-    await ctar.edit(name = f`Channels:{channelcount}`)
+    await ctar.edit(name = f'Channels:{channelcount}')
     
 @client.event
 async def on_guild_role_create(role):
     ctar = client.get_channel(561042914302754838)
     rolecount = ctar.guild.roles
-    await ctar.edit(name = f`Roles:{rolecount}`)
+    await ctar.edit(name = f'Roles:{rolecount}')
     
 @client.event
 async def on_guild_role_delete(role):
     ctar = client.get_channel(561042914302754838)
     rolecount = ctar.guild.roles
-    await ctar.edit(name = f`Roles:{rolecount}`)
+    await ctar.edit(name = f'Roles:{rolecount}')
     
 client.run(os.environ["BOT_TOKEN"])
 

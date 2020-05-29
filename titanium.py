6f37988bd6e418016740ea22cca9bb8c427c0eb8
Vitani <45301629+Vitani624@users.noreply.github.com>
2020-05-29 15:56:54 +0200

    Update titanium.py

--- titanium.py
+++ titanium.py
@@ -20,13 +20,13 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     ctar = client.get_channel(561044806919520256)
-    membercount = ctar.guild.members and not member.bot
+    membercount = ctar.guild.members.size and not member.bot
     await ctar.edit(name = f"Members: {membercount}")
 
 @client.event
 async def on_member_remove(member):
     ctar = client.get_channel(561044806919520256)
-    membercount = ctar.guild.members and not member.bot
+    membercount = ctar.guild.members.size and not member.bot
     await ctar.edit(name = f"Members: {membercount}")
 
 @client.event
